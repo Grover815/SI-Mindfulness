61c53217d249d353fc4fc290ad32eb6776b5c194
Brandon <bdstobie@wm.edu>
2025-11-09 17:25:53 -0500

    Added Comments to Motor.py

--- motor.py
+++ motor.py
@@ -11,52 +11,56 @@ class Stepper:
 		 [1,0,0,1]] # Full Step Logic Table
 
 
+	# Initializing Instance Variables 
 	def __init__(self, pins):
-		self.pins = pins # (a1, a2, b1, b2)
-		self.run = False
-		self.clockwise = True
-		self.speed = 0
-		self.step = 0
-		self.error = False
+		self.pins = pins # [a1, a2, b1, b2] input as list
+		self.run = False # Current state of motor
+		self.clockwise = True # Current direction of motor
+		self.speed = 0 # Current speed of motor
+		self.step = 0 # Current total step of motor 
+		self.error = False # Error handling
 
+		# GPIO Pin Setup
 		GPIO.setmode(GPIO.BCM)
 		for pin in pins:
 			GPIO.setup(pin,GPIO.OUT)
 
 
-	def step(self,steps,speed,clockwise=True):
-		slp = 1/(200*speed*60)
-		n = 1
-		if clockwise:
-			n=1
-		else:
-			n=-1
-		for i in range(0,steps):
-			GPIO.output(self.pins,FSL[n*(i%len(FSL))])
-			sleep(slp)
-
-	def start(self):
-		while not self.error:
-			if self.run:
-				self.move()
-		return "Motor Encountered Error"
-
+	# determines sleep time (s) based on the speed of the motor in rpm
 	def slp(self):
 		return 1/(200*self.speed*60)
 
+	# determines direction by travering FSL backwards (-1) or forwards (1)
 	def direction(self):
 		return -1 if not self.clockwise else 1
 
+	# Move function moves the motor one step
 	def move(self):
-		GPIO.output(self.pins,FSL[self.direction()*(self.step%len(FSL))])
+		GPIO.output(self.pins,FSL[self.direction()*(self.step%len(FSL))]) # outputs to each motor pin the logic table entry based on the mod (%) of the total self.step
 		self.step+=1
 		sleep(self.slp())
 
+	# stop function
 	def stop(self):
 		self.run = False
-		self.error = True
+
+	# Function to call move() N amount of times
+	def step(self,n):
+		for i in range(0,n):
+			self.move()
+
+	# Indefinetly run move()
+	def start(self):
+		while self.run:
+			self.move()
+			if self.error:
+				break
+		return "Motor Encountered Error"
 
 
+# this if statement is only run if the python file is run directly from terminal using python motor.py
+# this will not trigger when imported into another file
+# this isolation provides a good way to test the motor
 if __name__ == '__main__':
 	print("~Motor Test~")
 	pins = ["A1","A2",'B1',"B2"]
@@ -64,11 +68,17 @@ if __name__ == '__main__':
 		pins[i] = input(f"Pin {pins[i]}: ")
 	motor = Stepper(pins)
 	try:
+		newSpeed = True
 		while True:
-			speed = input("Speed (rpm): ")
-			motor.step(200,speed)
+			if newSpeed:
+				motor.speed = input("Speed (rpm): ")
+				newSpeed = False
+			motor.move(200) # 200 steps to complete a full rotation
 			print("One Rotation Complete")
+			if input("Change Speed (y/n): ") == 'y':
+				newSpeed= True
 	except KeyboardInterrupt:
+		print(f"Total Steps: {motor.step}")
 		print("Motor Test Terminated by User.")
 		motor.stop()
 		GPIO.cleanup()

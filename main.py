b98b58191a70f298224657dc956076420a2c0551
Brandon <bdstobie@wm.edu>
2025-11-16 19:42:34 -0500

    Changed from Threading to Multiprocessing.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import busio
 import adafruit_vl53l0x
 import adafruit_ssd1306
 
-from threading import Thread
+from multiprocessing import Process
 
 #Initiliaze I2C
 i2c = busio.I2C(board.SCL,board.SDA)
@@ -27,13 +27,16 @@ disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 motor1 = Stepper([0,0,0,0])
 motor2 = Stepper([0,0,0,0])
 
+# Pass display object to GUI Wrapper that contains neat functions to output to display
+#GUI = DisplayGUI(disp)
+
 
 def main():
-	t1 = Thread(target=motor1.start)
-	t2 = Thread(target=motor2.start)
+	p1 = Process(target=motor1.start)
+	p1.start()
+	p2 = Process(target=motor2.start)
+	p2.start()
 
-	# Pass display object to GUI Wrapper that contains neat functions to output to display
-	GUI = DisplayGUI(disp)
 	while True:
 		if input("Run? (y/n)") == 'y':
 			motor1.running = True
@@ -42,12 +45,11 @@ def main():
 			motor1.running = False
 			motor2.running = False
 			break
-		GUI.writeMessage()
 	
 
 
-	t1.join()
-	t2.join()
+	p1.join()
+	p2.join()
 
 
 

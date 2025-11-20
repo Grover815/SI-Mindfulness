b2357f6cec633c59e10ef453dddcea147debc166
Brandon Stobie <bdstobie@wm.edu>
2025-11-20 17:07:35 -0500

    Lab reworking of multiple sensors.

--- VL53L0X.py
+++ VL53L0X.py
@@ -6,30 +6,36 @@ import busio
 import adafruit_vl53l0x
 from time import sleep
 import math
+import RPi.GPIO as GPIO
+import time
+
 
 
 
 class Gesture():
 
-	max_distance = 200 # Maximum distance to detect object
-	min_distance = 0 # Minimum distance to detect object
-
+	
 	sensor_distance = 20 # distance between first and last sensor
 
 	def __init__(self,xshut,i2c):
 		self.vl53 = []
-		self.position = [0 for i in range(0,len(xshut))]
+		self.pos = [0 for i in range(0,len(xshut))]
 		self.time = [0 for i in range(0,len(xshut))]
 		self.speed = 0
 		self.direction = 0 # 0 for left, 1 for right
+		self.i2c = i2c
+		self.max_distance = 100 # Maximum distance to detect object
+		self.min_distance = 0 # Minimum distance to detect object
 		
 		GPIO.setmode(GPIO.BCM)
-		for i in range(0,len(xShut)):
-			GPIO.setup(xShut[i], GPIO.OUT, initial=GPIO.LOW)
-		for i in range(len(xShut)):
-			self.vl53.append(adafruit_vl53l0x.VL53L0X(i2c))
+		for pin in xshut:
+			print(pin, "Setup")
+			GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+		for i in range(0,len(xshut)):
+			#GPIO.output(xshut[i],GPIO.HIGH)
+			self.vl53.append(adafruit_vl53l0x.VL53L0X(self.i2c,address=0x29))
 			self.vl53[i].start_continuous()       # Hardware/power limitations of using continuous mode?
-			self.vl53[i].set_address(i + 0x30)
+			#self.vl53[i].set_address(i + 0x29)
 
 	def distance(self):
 		distances = [0 for i in range(0,len(self.vl53))]
@@ -38,16 +44,16 @@ class Gesture():
 		return distances
 
 	def is_valid(self,distance):
-		return (distance > self.min_distance) and (self.max_distance <200)
+		return (distance > self.min_distance) and (distance < self.max_distance)
 
 	def position(self):
 		for i, sensor in enumerate(self.vl53):
 			distance = sensor.range
-			if is_valid(distance):
-				self.position[i] = 1
-				self.time = time.time()
+			if self.is_valid(distance):
+				self.pos[i] = 1
+				self.time[i] = time.time()
 			else:
-				self.position[i] = 0
+				self.pos[i] = 0
 
 	def direction(self):
 		self.direction = 1 if (self.time[0]-self.time[-1] < 0) else 0 
@@ -67,21 +73,25 @@ class Gesture():
 		return ret
 
 
-	def stop():
-		for sensor in vl53:
-        	sensor.stop_continuous() # Implemenet error handling if other functions are called while continuous ranging is off
+	def stop(self):
+		for sensor in self.vl53:
+			sensor.stop_continuous() # Implemenet error handling if other functions are called while continuous ranging is off
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
+	print("Initializing")
 	#Initiliaze I2C
-	i2c = busio.I2C(board.SCL,board.SDA)
-	pins = [] # Pins for Shutoff pins
+	i2c = board.I2C()
+	sensor = adafruit_vl53l0x.VL53L0X(i2c,address=0x29)
+	sensor.start_continuous()
+	print(sensor.range)
+	pins = [5] # Pins for Shutoff pins
 	wave = Gesture(pins,i2c)
 	try:
-		update = True
-		while update:
-			update = wave.update()
-			print(wave.distance()) # wave.position
+		while True:
+			wave.position()
+			print(wave.pos) # wave.position
+			sleep(1)
 	except KeyboardInterrupt:
 		wave.stop()
 		GPIO.cleanup()

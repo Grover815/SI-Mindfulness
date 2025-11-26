b3e57a4d70f55d503d4d452e4e50b367ef3560e2
Brandon <bdstobie@wm.edu>
2025-11-26 00:15:58 -0500

    Added error handling, logging, and acceleration profile and removed redundant functions.

--- motor.py
+++ motor.py
@@ -2,6 +2,7 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+from logs import setup_logger
 
 class Stepper:
 	
@@ -10,43 +11,47 @@ class Stepper:
 		 [0,1,0,1],
 		 [1,0,0,1]] # Full Step Logic Table
 
+	# Tested range
+	max_speed = 3 # rps
+	min_speed = 0.1 # rps
+
+	deceleration = 0.1 # how many rps to decrease each step
+	acceleration = 0.1 # how many rps to increase each step
+
+
 
 	# Initializing Instance Variables 
 	def __init__(self,pins,connection):
 		self.pins = pins # [a1, a2, b1, b2] input as list
 		self.running = True # Current state of motor
-		self.clockwise = True # Current direction of motor
-		self.speed = 0 # Current speed of motor
+		self.spd = 0 # Current velocity of motor, positive is clockwise, negative is counterclockwise
+		self.speed_max = 0 # Maximum speed achieved
 		self.step = 0 # Current total step of motor 
-		self.error = False # Error handling
-		self.coast = True
 		self.conn = connection
 
 		# GPIO Pin Setup
 		GPIO.setmode(GPIO.BCM)
-		print(self.pins)
 		for pin in self.pins:
 			GPIO.setup(pin,GPIO.OUT)
 
 
 	# determines sleep time (s) based on the speed of the motor in rps
 	def slp(self):
-		return 1/(200*self.speed)
+		try:
+			return 1/(200*abs(self.spd))
+		except ZeroDivisionError:
+			return 0
 
-	# determines direction by travering FSL backwards (-1) or forwards (1)
-	def direction(self):
-		return -1 if not self.clockwise else 1
+	def sign(self,speed):
+		return 1 if speed > 0 else -1
 
 	# Move function moves the motor one step
 	def move(self):
-		GPIO.output(self.pins,self.FSL[self.direction()*(self.step%len(self.FSL))]) # outputs to each motor pin the logic table entry based on the mod (%) of the total self.step
+		GPIO.output(self.pins,self.FSL[self.sign(self.spd)*(self.step%len(self.FSL))]) # outputs to each motor pin the logic table entry based on the mod (%) of the total self.step
 		#print(self.FSL[self.direction()*(self.step%len(self.FSL))])
 		self.step+=1
 		sleep(self.slp())
 
-	# stop function
-	def stop(self):
-		self.run = False
 
 	# Function to call move() N amount of times
 	def steps(self,n):
@@ -55,27 +60,40 @@ class Stepper:
 
 	# Indefinetly run move()
 	def start(self):
-		while self.running: # not self.runninng.is_set()
+		while True:
 			self.move()
-			if self.coast:
-				self.speed -= 0.1 # Deacceleration strategry?
-			if self.speed <= 0.5:
-				break
-		print("Motor stopped.")
 
 	# Multiprocessing implementation of start()
 	def run(self):
+		logger = setup_logger()
+		logger.info(f"{current_process().name} running...")
+		targetSpeed = None
 		while True:
-			if self.speed> 0:
+			
+			if self.spd > targetSpeed:
+				self.spd -= self.deceleration
+			elif self.spd < targetSpeed:
+				self.spd += self.acceleration
+
+			if abs(self.spd) >= min_speed: # if it is set below min speed the motor will stop i.e. to 0
 				self.move()
+
 			if self.conn.poll():
 				msg = self.conn.recv()
+				logger.info(f"{current_process().name} Received: {msg}")
 				if msg == "END":
 				    break
+				if msg == "GET":
+					self.conn.send([self.step,self.speed_max])
 				if type(msg) == float: # More validation on speed input? In main logic perhaps
-					#print(msg)
-					self.speed = abs(msg)
-					self.clockwise = True if msg > 0 else False
+					if abs(msg) > self.speed_max:
+						self.speed_max = abs(msg)
+					targetSpeed = msg
+					logger.info(f"Current Speed: {self.spd}, Target Speed: {targetSpeed}, Clockwise: {self.clockwise}")
+					if self.spd == 0: # if a target speed is received when motor is stopped (self.spd = 0), give it a little speed to start the move() conditional
+						self.spd = self.sign(msg)*self.min_speed
+
+		logger.debug(f"{current_process().name} connection closed")
 		self.conn.close()
 
 
